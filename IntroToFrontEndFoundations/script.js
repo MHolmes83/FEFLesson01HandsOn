e15c77e55bc42a24a66a3133ea9f8efee3562958
Morgaan Holmes <morgaan.holmes@my.scitexas.edu>
2023-09-18 03:23:05 -0400

    Intro to Front-End Foundations
    Started a new Node project and ran some simple functions to practice basic code and printed the results to the browser console.

---
+++ IntroToFrontEndFoundations/script.js
@@ -0,0 +1,26 @@
+function add(a, b) {
+    return a + b;
+}
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+var person = {
+    name: "Andrew",
+    age: 23,
+    hairColor: "Black",
+    eyeColor: "Green"
+}
+
+function addNumbers(e, f) {
+    var g = e + f;
+    console.log(g);
+}
+
+console.log("Hello, World!");
+console.log(add(1, 5));
+console.log(4 + 5);
+console.log(Date());
+console.log(numbers);
+console.log(person);
+console.log(person.age < 30);
+addNumbers(43, 623);
